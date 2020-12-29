3caf177dcbf79f1d00b9ae8b2d364774d90b9250
Imad <imadaufil@gmail.com>
2020-12-29 23:46:19 +0100

    feat: fragment class

---
+++ app/src/main/java/com/example/mysteriousapp/ArtsSectionFragment.java
@@ -0,0 +1,34 @@
+package com.example.mysteriousapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ArtsSectionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ArtsSectionFragment extends Fragment {
+
+
+    public ArtsSectionFragment() {
+        // Required empty public constructor
+    }
+
+    public static ArtsSectionFragment newInstance() {
+        return new ArtsSectionFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_arts_section, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/mysteriousapp/TopStoriesFragment.java
@@ -0,0 +1,30 @@
+package com.example.mysteriousapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TopStoriesFragment extends Fragment {
+
+
+    public TopStoriesFragment() {
+        // Required empty public constructor
+    }
+
+    public static TopStoriesFragment newInstance() {
+        return new TopStoriesFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top_stories, container, false);
+    }
+
+}
