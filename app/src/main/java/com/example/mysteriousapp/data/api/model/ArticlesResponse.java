01c052cf81c321fb03131087570486b991a0fe92
imadaufil <imadaufil@gmail.com>
2021-01-02 16:57:50 +0100

    refactor: articleshomeresponse -> articlesresponse

--- app/src/main/java/com/example/mysteriousapp/data/api/model/ArticlesResponse.java
+++ app/src/main/java/com/example/mysteriousapp/data/api/model/ArticlesResponse.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class ArticlesHomeResponse {
+public class ArticlesResponse {
 
     @SerializedName("section")
     private String section;
